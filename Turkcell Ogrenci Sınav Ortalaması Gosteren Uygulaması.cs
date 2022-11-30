6971bc0364b94e8cc7459fd9851c67e0027ecc69
Berat Günaydın <116026350+BeratG28@users.noreply.github.com>
2022-11-30 14:45:08 +0300

    Add files via upload

---
+++ Turkcell Ogrenci Sınav Ortalaması Gosteren Uygulaması.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            String ad, soyad, bolum, ders;
+            int s1, s2, s3, ort;
+
+            ad= "Berat";
+            soyad= "Gunaydın";
+            bolum= "Elektrik";
+            ders= "algoritma";
+
+            s1= 65;
+            s2= 75;
+            s3= 88;
+            ort= (s1 + s2 + s3) / 3;
+
+            Console.WriteLine("***** Ogrenci Bilgi Sistemi *****");
+
+            Console.WriteLine();
+            Console.WriteLine("Ogrenci Adı Soyadı:" + ad + " " + soyad);
+            Console.WriteLine("Bölüm: " + bolum );
+            Console.WriteLine("Ders: " + ders);
+            Console.WriteLine();
+            Console.WriteLine("sınav 1: " + s1 );
+            Console.WriteLine("sınav 2: " + s2 );
+            Console.WriteLine("sınav 3: " + s3 );
+            Console.WriteLine("Ortalamanız:"+ ort);
+
+            Console.WriteLine("***** Ogrenci Bilgi Sistemi *****");
+
+            Console.Read();
+
+
+
+
+
+        }
+    }
+}
